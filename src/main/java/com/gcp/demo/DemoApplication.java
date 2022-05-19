1697b8aa9d121e536eadd98bb51504b1e35a38c1
Elier <elier.moram@gmail.com>
2022-05-19 10:06:10 -0500

    logger

--- src/main/java/com/gcp/demo/DemoApplication.java
+++ src/main/java/com/gcp/demo/DemoApplication.java
@@ -1,15 +1,18 @@
 package com.gcp.demo;
 
+import java.util.logging.Logger;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+	private static final Logger logger = Logger.getLogger(DemoApplication.class.getName());
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		
-		System.out.println("*************LISTO");
+		logger.info("Strat Demo..... ");
 	}
 
 }
